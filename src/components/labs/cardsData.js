be36a03d8bc922e6850ab36821cd9d2ece60029c
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-10-08 16:16:17 +0700

    update

--- src/components/labs/cardsData.js
+++ src/components/labs/cardsData.js
@@ -3,7 +3,7 @@ const cardsData = [
     title: "Mind Reader. 🤯",
     desc: "This experiment will tell you the truth that no mind is safe. Try it if you cant believe it.",
     href: "/labs/mindreader",
-    img: "http://localhost:3000/mindreader.png",
+    img: "https://raiyanlabs.vercel.app/mindreader.png",
     stacks: [
       { name: "react", color: "bg-blue-500" },
       { name: "tailwind", color: "bg-gray-900" },
@@ -14,7 +14,7 @@ const cardsData = [
     title: "Speech To Text. 🎙",
     desc: "Speech To Text is a tool to help you type, this tool is perfect for you who are lazy about typing.",
     href: "/labs/speechtotext",
-    img: "http://localhost:3000/speechtotext.png",
+    img: "https://raiyanlabs.vercel.app/speechtotext.png",
     stacks: [
       { name: "react", color: "bg-blue-500" },
       { name: "tailwind", color: "bg-gray-900" },
@@ -25,7 +25,7 @@ const cardsData = [
     title: "TicTacToe. 🎮",
     desc: "Everyone already knows this game, i created this by following the react.dev tutroial.",
     href: "/labs/tictactoe",
-    img: "http://localhost:3000/tictactoe.png",
+    img: "https://raiyanlabs.vercel.app/tictactoe.png",
     stacks: [
       { name: "react", color: "bg-blue-500" },
       { name: "tailwind", color: "bg-gray-900" },
@@ -36,7 +36,7 @@ const cardsData = [
     title: "Decision Maker. 🤔",
     desc: "Still confused to make a decision, try Decision Maker it will help you.",
     href: "/labs/decisionmaker",
-    img: "http://localhost:3000/decisionmaker.png",
+    img: "https://raiyanlabs.vercel.app/decisionmaker.png",
     stacks: [
       { name: "react", color: "bg-blue-500" },
       { name: "tailwind", color: "bg-gray-900" },
