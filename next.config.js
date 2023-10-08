a4e8e3732fdd51bff993dd32a45ffce232da5459
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-10-08 16:15:09 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["media.tenor.com", "localhost"],
+    domains: ["media.tenor.com", "raiyanlabs.vercel.app"],
   },
 };
 
