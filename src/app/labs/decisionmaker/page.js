04d4827056487a00c5bada4bf15f3aca8a67e2ee
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-11-28 16:51:15 +0700

    endpoint update

--- src/app/labs/decisionmaker/page.js
+++ src/app/labs/decisionmaker/page.js
@@ -38,18 +38,26 @@ const DecisionMaker = () => {
   const apiCall = async (data) => {
     const response = await axios.post(
       "https://chatbb.free2gpt.xyz/api/generate",
-      {
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "user",
-            content: data,
-          },
-        ],
-      },
+      `{
+        "messages":[{"role":"user","content": ${data}}],
+      }`,
       {
         headers: {
-          "Content-Type": "application/json",
+          authority: "chatbb.free2gpt.xyz",
+          accept: "*/*",
+          "accept-language": "en-US,en;q=0.9,id;q=0.8",
+          "content-type": "text/plain;charset=UTF-8",
+          origin: "https://chatbb.free2gpt.xyz",
+          referer: "https://chatbb.free2gpt.xyz/",
+          "sec-ch-ua":
+            '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
         },
       }
     );
